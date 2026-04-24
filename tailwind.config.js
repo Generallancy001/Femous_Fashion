d5f5a72e0888878701c96446157a142c7b0554c8
Ojo Oluwafemi Emmanuel <167341159+Generallancy001@users.noreply.github.com>
2026-04-24 08:27:15 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+  './index.html',
+  './src/**/*.{js,ts,jsx,tsx}'
+],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          dark: '#111111',
+          light: '#FAFAFA',
+          gold: '#C9A96E',
+          gray: '#F3F4F6',
+          grayDark: '#4B5563'
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      }
+    },
+  },
+  plugins: [],
+}
